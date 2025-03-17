dcc45397d8554648a52b323e29513b566707eb07
MinSeugn <66229776+MinSeugn@users.noreply.github.com>
2025-03-18 08:59:25 +0900

    [refactor] 날짜 단위로 채팅 세션 관리 (#9)
    Co-authored-by: 조항범

--- frontend/user/sidebar.py
+++ frontend/user/sidebar.py
@@ -1,12 +1,14 @@
 import streamlit as st
+from datetime import datetime
 
 def display_sidebar():
     with st.sidebar:
         if st.button("새 대화 시작"):
             st.session_state['chat_count'] += 1
-            new_chat_name = f"대화{st.session_state['chat_count']}"
+            new_chat_name = f"{datetime.now().strftime('%Y.%m.%d')} 대화{st.session_state['chat_count']}"
             st.session_state['current_chat'] = new_chat_name
             st.session_state['chats'][new_chat_name] = []
+            st.session_state['chat_dates'][new_chat_name] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 새 대화 날짜 기록
 
         previous_chats = list(st.session_state['chats'].keys())
         st.subheader("이전 대화")
