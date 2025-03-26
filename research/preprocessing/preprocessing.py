49a8c521d4cf0d8204f3e104dab94fce72df113e
always-spring01 <hangbum01@gmail.com>
2025-03-26 09:51:45 +0900

    [feat] : sentencePiece 학습 함수 생성

--- research/preprocessing/preprocessing.py
+++ research/preprocessing/preprocessing.py
@@ -2,6 +2,8 @@
 preprocessing.py : 전처리 함수를 정의한 파일
 - 최신화 : 2025.03.26
 '''
+import sentencepiece as spm
+import os
 
 # txt 파일 불러오는 함수
 def read_text(file_path):
@@ -33,7 +35,19 @@ def word_preprocessing(file_path, window_size=10, slide=3):
 
     return result
 
+# SentencePiece 학습 함수
+def training_spm(file_path, model_name="default"):
+    os.makedirs("./sentencepiece", exist_ok=True)
+    spm.SentencePieceTrainer.Train(
+        input=file_path,
+        model_prefix=f"./sentencepiece/{model_name}",
+        character_coverage=1.0,
+        vocab_size=32000, # 수정 가능
+        model_type="unigram" # 기본 값
+    )
+    print(f"[training_spm] : Training Complete to ./sentencepiece/{model_name}")
 
 # Debug
 if __name__ == '__main__':
     print(word_preprocessing(r"./sample_text.txt", 10, 3))
+    training_spm(r"./ko_wiki_text.txt", model_name="ko_wiki_text_model")
