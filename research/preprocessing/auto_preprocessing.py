7cf21c86549cbe08abbae7d7cb3e4e345c1f78ef
JuYeobOh <to765@kookmin.ac.kr>
2025-04-02 15:06:52 +0900

    [feat] : 예외 처리 및 결과 저장 방식 수정

--- research/preprocessing/auto_preprocessing.py
+++ research/preprocessing/auto_preprocessing.py
@@ -16,7 +16,7 @@ def delete_newline(text):
     return text.replace('\n', ' ')
 
 # 단어 단위 전처리 방식
-def word_preprocessing(data_dict, window_size=10, slide=3):
+def word_preprocessing(data_dict, window_size=10, slide=1):
     result_dict = {}
 
     for doc in data_dict.keys():
@@ -24,19 +24,25 @@ def word_preprocessing(data_dict, window_size=10, slide=3):
 
         text = delete_newline(data_dict[doc])
         words = text.split()
+
+        # 미만인거 그냥 리턴
+        if len(words) < window_size:
+            result_dict[doc].append(text)
+            continue
+
         for i in range(0, len(words), slide):
             window = words[i:i+window_size]
-            if window:
+            if len(window) == window_size:
                 sentence = ' '.join(window)
                 result_dict[doc].append(sentence)
 
-        # 나머지 단어 청크 추가
-        last = (len(words) - window_size) % slide
-        if last != 0:
-            window = words[-window_size:]
-            sentence = ' '.join(window)
-            if sentence not in result_dict[doc]:
-                result_dict[doc].append(sentence)
+        # # 나머지 단어 청크 추가
+        # last = (len(words) - window_size) % slide
+        # if last != 0:
+        #     window = words[-window_size:]
+        #     sentence = ' '.join(window)
+        #     if sentence not in result_dict[doc]:
+        #         result_dict[doc].append(sentence)
 
     return result_dict
 
@@ -61,33 +67,48 @@ def autoencode(result_path=r"./result", model=SentenceTransformer("jhgan/ko-sber
         with open(f"{result_path}/encode/{path}", "wb") as f:
             pickle.dump(encoded_dict, f)
 
-def autocheck(result_path = r"./result", target_folder1 = r"./result/encode", threshold1 = 0.85, threshold2 = 0.10):
+def autocheck(result_path="./result", target_folder1="./result/encode", threshold1=0.85, threshold2=0.10):
     path_list = os.listdir(target_folder1)
+
     for path in tqdm(path_list, desc="Checking", ncols=100):
         with open(f"{target_folder1}/{path}", "rb") as f:
             data = pickle.load(f)
 
-        # 유사도 계산
-        # sim_dict = {}
-        file_list = list(data.keys())
+        files = list(data.keys())
 
-        for i in range(len(file_list)):
-            pivot = np.array(data[file_list[i]])
+        # pivot: ori.txt 파일 / target: 나머지
+        pivot_files = [f for f in files if f.endswith("ori.txt")]
+        target_files = [f for f in files if not f.endswith("ori.txt")]
+
+        all_rows = []
+
+        for pivot_file in pivot_files:
+            pivot = np.array(data[pivot_file])
             pivot = torch.tensor(pivot, dtype=torch.float32, device='cuda')
 
-            for j in range(i+1, len(file_list)):
-                target = np.array(data[file_list[j]])
+            for target_file in target_files:
+                target = np.array(data[target_file])
                 target = torch.tensor(target, dtype=torch.float32, device='cuda')
 
                 sim_matrix = torch.mm(pivot, target.T)
                 cnt = (sim_matrix >= threshold1).sum(dim=1).gt(0).sum().item()
                 label = 1 if cnt >= len(pivot) * threshold2 else 0
-            
-                row = pd.DataFrame([{
-                    "file1": file_list[i],
-                    "file2": file_list[j],
+
+                row = {
+                    "file1": pivot_file,
+                    "file2": target_file,
                     "similarity": cnt / len(pivot) * 100,
                     "label": label
-                }])
+                }
 
-                row.to_csv(f"{result_path}/check/{path}_{threshold1}_{threshold2}.csv", mode='a', header=False, index=False)
+                all_rows.append(row)
+
+        df = pd.DataFrame(all_rows)
+        
+        base_filename = os.path.splitext(path)[0]
+        output_path = f"{result_path}/check/{base_filename}_{threshold1}_{threshold2}.csv"
+        
+        df.to_csv(output_path, index=False)
+
+if __name__ == "__main__":
+    autocheck()
