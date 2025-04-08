e69483535ab65bfd006d25c21ddeea1dc9155606
always-spring01 <hangbum01@gmail.com>
2025-04-08 17:42:32 +0900

    [hotfix] 사용하지 않는 데이터와 관련 코드 삭제

--- research/data/question_pair/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df_test = pd.read_csv(r"./kor_Pair_test.csv")
-df_train = pd.read_csv(r"./kor_pair_train.csv")
-df_total = pd.concat([df_test, df_train], ignore_index=True)
-df_total = df_total[['question1', 'question2', 'is_duplicate']]
-df_total.to_csv(r"./kor_pair_question.csv")
-
-print(df_total.head(10))
