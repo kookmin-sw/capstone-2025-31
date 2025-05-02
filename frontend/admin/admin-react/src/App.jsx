e7707d570fdced9a1eb0fcc7d75937a0bc18e893
zia1014 <149453831+zia1014@users.noreply.github.com>
2025-05-02 20:59:16 +0900

    [feat] 관리자 페이지 로그인 화면 구현

--- frontend/admin/admin-react/src/App.jsx
+++ frontend/admin/admin-react/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-import AdminPage from './AdminPage'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './Login'
+import Dashboard from './Dashboard'
 
 function App() {
   return (
-    <div>
-      <AdminPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
